b9eaff63d6879599b2d50639bfc7f5e63d3fb6b6
Francisco Donadio <francisco.donadio@hotmail.com>
2022-07-03 15:19:51 -0300

    Initial Commit

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Models.swift
+//  Flash Chat iOS13
+//
+//  Created by Francisco Donadio on 04/05/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
